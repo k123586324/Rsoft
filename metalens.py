2dddfb2b44752e2d82b4b3d6cc1991581a0f8204
k123586324 <cg1436@hotmail.com>
2022-05-16 17:22:33 +0800

    Update 2022/05/16

--- metalens.py
+++ metalens.py
@@ -2,33 +2,47 @@
 from rstools import *
 from math import *
 
-def rsoft_main(ind_file='',
-               diameter_ratio_file='',
-               Period=0.24,
-               D=10):
+def rsoft_main(ind_file=''):
 
   # create a circuit
   c=RSoftCircuit()
-
-  # set symbols
-  c.set_symbol('Thickness', 1)
-  c.set_exports('Thickness')
-
-  # read data file
-  diameter_ratio_data = RSoftUserFunction()
-  diameter_ratio_data.read(diameter_ratio_file)
-  
-  # calculate array dimension
-  N = 2*int((D/2)/Period)+1
-
-  # build metalens
-  for i in range(N):
-    for j in range(N):
-      x = (i-(N-1)/2)*Period
-      y = (j-(N-1)/2)*Period
-      r = sqrt(x**2+y**2)
-      ratio = diameter_ratio_data.eval(r)
-      c.add_segment( (x,y,0), (0,0,'Thickness'), (ratio*Period, ratio*Period) )
+  array=[
+    0.168,
+    0.167,
+    0.165,
+    0.163,
+    0.161,
+    0.157,
+    0.154,
+    0.149,
+    0.144,
+    0.138,
+    0.132,
+    0.124,
+    0.114,
+    0.101,
+    0.081,
+    0.22,
+    0.209,
+    0.2,
+    0.19,
+    0.181,
+    0.17,
+    0.158,
+    0.145,
+    0.131,
+    0.112,
+    0.083,
+    0.213,
+    0.199,
+    0.185,
+    0.17]
+  Period=0.25
+  c.add_segment( (0,0,0.5), (0,0,1.1), (0.169, 0.169) )
+  for i in range(len(array)):
+    x = (i+1)*Period
+    c.add_segment( (x,0,0.5), (0,0,1.1), (array[i], array[i]) )
+    c.add_segment((-x, 0, 0.5), (0, 0, 1.1), (array[i], array[i]))
 
   # save circuit to file
   c.write(ind_file)
